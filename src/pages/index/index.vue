8eb80093ea750d556158341aaac0d73509617411
ryan <ryanscode@163.com>
2026-02-20 17:59:16 +0800

    t003 use native tabbar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,12 @@
 <template>
   <view class="content tabbar-page">
-    <text>首页内容</text>
-  </view>
-  <view>
-    <CustomTabbar />
+    <text>首页</text>
   </view>
 </template>
 
 <script setup lang="ts">
-import CustomTabbar from '@/components/CustomTabbar.vue'
-import { useTabbarStore } from '@/stores'
-import { onShow } from '@dcloudio/uni-app'
+import { onShow } from '@dcloudio/uni-app';
 
-const tabbarStore = useTabbarStore()
-
-// 页面显示时确保tabbar可见
 onShow(() => {
-  //tabbarStore.toggleTabbar(true)
 })
-
-// 示例：更新购物车角标
-// const updateCartBadge = () => {
-//   const cartCount = Math.floor(Math.random() * 100)
-//   tabbarStore.updateBadge(2, cartCount) // 索引2是购物车
-// }
 </script>
